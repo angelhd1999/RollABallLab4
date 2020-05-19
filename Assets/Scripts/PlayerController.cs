11dfdfdbcb476895883b73bfaabe2fd8d6620333
angelhd1999 <angelhd1999@gmail.com>
2020-05-19 12:26:55 +0200

    RollABall Copy commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float powerJump = 7;
+    public int pickUpsToWin = 21;
+    public Text countText;
+    public Text winText;
+    public LayerMask groundLayers;
+    public SphereCollider col;
+
+    private Rigidbody rb;
+    private int count;
+    private AudioSource source;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * powerJump, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+        
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            Debug.Log("Bang");
+            source.Play();
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= pickUpsToWin)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public GameObject pickUp;
+    public float translationSpeed = 0.25f;
+    public bool debugshow;
+
+    private bool goingUp;
+    private float initPosition;
+
+    void Start()
+    {
+        goingUp = true;
+        initPosition = pickUp.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        Levitate();
+    }
+
+    void Levitate()
+    {
+        if ((pickUp.transform.position.y < initPosition + 0.25f) && goingUp)
+        {
+            transform.Translate(0, translationSpeed * Time.deltaTime, 0, Space.World);
+        }
+        else if ((pickUp.transform.position.y > initPosition) && !goingUp)
+        {
+            transform.Translate(0, -translationSpeed * Time.deltaTime, 0, Space.World);
+        }
+        else
+        {
+            goingUp = !goingUp;
+        }
+    }
+
+}
